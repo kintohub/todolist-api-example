554108f5fe2589b1a824c17b0df91d5608776218
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2019-07-11 16:00:38 +0800

    feat(server) enable log errors

--- app/index.js
+++ app/index.js
@@ -10,7 +10,8 @@ import TodoCollection from './models/TodoCollection'
 const server = Hapi.server({
   host: process.env.SERVER_HOST || '0.0.0.0',
   port: process.env.SERVER_PORT || '8000',
-  routes: { cors: true } // So that localhost from different ports can connect.
+  routes: { cors: true }, // So that localhost from different ports can connect.
+  debug: { request: ['error'] } // Always log errors.
 })
 
 /**
